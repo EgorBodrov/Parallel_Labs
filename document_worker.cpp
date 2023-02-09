014910ceaee4bddb742f8bdf378a68fdf5da0161
BekusovMikhail <63633043+BekusovMikhail@users.noreply.github.com>
2023-02-10 00:13:12 +0300

    Add files via upload

---
+++ document_worker.cpp
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define QUANTITY 1000
+#define PARAMETERS_COUNT 5
+#define dt 0.5
+#define GRAVITY_CONSTANT 0.000000000066743
+#define STEPS 10000
+
+struct MatPoint {
+    double mass;
+    double x;
+    double y;
+    double vx;
+    double vy;
+    double Fx;
+    double Fy;
+};
+
+void create_document(int quantity, char * document_name)
+{
+  srand(time(NULL));
+
+  FILE *fp;
+
+  if ((fp = fopen(document_name, "w")) == NULL)
+  {
+    printf("Не удалось открыть файл");
+
+    return;
+  }
+  fprintf(fp, "%d\n", QUANTITY); 
+  for(int i=0; i<QUANTITY; i++){
+
+    // масса, x, y, z, vx, vy, vz
+    for (int j=0; j<PARAMETERS_COUNT; j++)
+    {
+      double r=0;
+      if (j>2){
+        r = rand() % 10;
+      }
+      else if (j<=2 and j>0){
+        r = rand() % 20000;
+      }
+      else{
+        r = rand()*10000;
+      }
+      fprintf(fp, "%f ", r); 
+    }
+    
+
+    fprintf(fp, "\n");
+  }
+
+  fclose(fp);
+}
+
+MatPoint * get_mat_points(MatPoint * mat_points, char * document_name)
+{
+
+    FILE *fp;
+    char str[100];
+
+    char *estr;
+
+    fp = fopen(document_name, "r");
+
+    if (fp == NULL)
+    {
+        return NULL;;
+    }
+
+    estr = fgets(str, sizeof(str), fp);
+    int quantity = atoi(str);
+    // printf("%d", quantity);
+
+    // MatPoint mat_points[QUANTITY];
+
+    for (int row_index=0; row_index<quantity; row_index++)
+    {
+        estr = fgets(str, sizeof(str), fp);
+
+        if (estr == NULL)
+        {
+            if (feof(fp) != 0)
+            {
+                break;
+            }
+            else
+            {
+                break;
+            }
+        }
+        char * token = strtok(str, " ");
+        int column = 0;
+        // mat_points[row_index] = new int [PARAMETERS_COUNT];
+        // mat_points[row_index][column] = atoi(token);
+
+        mat_points[row_index].mass = atoi(token);
+        
+        column++;
+        
+        while( token != NULL ) {
+          token = strtok(NULL, " ");
+          switch (column)
+          {
+          case 1:
+            mat_points[row_index].x = atoi(token);
+            break;
+          case 2:
+            mat_points[row_index].y = atoi(token);
+            break;
+          case 3:
+            mat_points[row_index].vx = atoi(token);
+            break;
+          case 4:
+            mat_points[row_index].vy = atoi(token);
+            break;
+          
+          default:
+            break;
+          }
+            column++;
+        }
+        // printf("%s", str);
+    }
+
+    fclose(fp);
+
+    return mat_points;
+}
+
+void print_mat_points(MatPoint * mat_points)
+{
+    for (int row_index=0; row_index<QUANTITY; row_index++)
+    {
+        printf("%f ", mat_points[row_index].mass);
+        printf("%f ", mat_points[row_index].x);
+        printf("%f ", mat_points[row_index].y);
+        printf("%f ", mat_points[row_index].vx);
+        printf("%f ", mat_points[row_index].vy);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void print_mat_points_in_file(MatPoint * mat_points, char * document_name, int step)
+{
+
+    FILE *fp;
+  
+    if ((fp = fopen(document_name, "a")) == NULL)
+    {
+      printf("Не удалось открыть файл");
+      return;
+    }
+    fprintf(fp, "%f\n", (float)(step+1)*dt);
+    for(int i=0; i<QUANTITY; i++){
+
+      fprintf(fp, 
+      "%f %f ",
+      mat_points[i].x,
+      mat_points[i].y
+      );
+    }
+    fprintf(fp, "\n");
+
+    fclose(fp);
+}
